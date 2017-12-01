57bcd0ca584b844370566cc8e7524d6f492a16d4
Chris Chasm <chris@chasm.solutions>
2017-12-01 14:37:20 -0700

    Refactoring includes folder

--- includes/login/wplogin_redirect_control.php
+++ includes/login/wplogin_redirect_control.php
@@ -1,8 +1,8 @@
 <?php
     // This assumes that this files sits in "wp-content/plugins/peters-login-redirect/wplogin_redirect_control.php" and that you haven't moved your wp-content folder
-    if( file_exists( '../../../../wp-load.php' ) )
+    if( file_exists( '../../../../../wp-load.php' ) )
     {
-        include '../../../../wp-load.php';
+        include '../../../../../wp-load.php';
     }
     else
     {
