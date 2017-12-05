81229bd08a2333ae9f86ae4694865072ef81066b
Chris Chasm <chris@chasm.solutions>
2017-12-05 11:40:11 -0800

    More foundations 6.4 settings applied
    I missed some 6.4 settings that should have been converted.

--- template-zume-about.php
+++ template-zume-about.php
@@ -14,9 +14,9 @@ Template Name: Zume About
 
     <div id="inner-content">
 
-        <div class="grid-x">
+        <div class="grid-x ">
 
-            <div class="large-8 large-offset-2 small-12 cell">
+            <div class="large-8 large-offset-2 small-12 cell ">
 
                 <!-- Place video here -->
                 <div class="responsive-embed widescreen">
@@ -25,8 +25,8 @@ Template Name: Zume About
                             width="640"></iframe>
                 </div>
 
-                <div class="grid-x">
-                    <div class="cell">
+                <div class="grid-x row">
+                    <div class="cell ">
                         <p style="font-size: 150%; margin: 20px 0 40px 0; text-align: center;">Zúme Training is an
                             on-line and in-life learning experience designed for small groups who follow Jesus to learn
                             how to obey His Great Commission and make disciples who multiply.</p>
@@ -36,10 +36,10 @@ Template Name: Zume About
             </div>
         </div>
 
-        <div class="grid-x">
-            <div class="cell" style="background-color:#323A68; padding: 1.5em 0;">
-                <div class="grid-x grid-margin-x grid-margin-y ">
-                    <div class="large-8 large-offset-2 cell" style="color: white;">
+        <div class="grid-x row padding-bottom">
+            <div class="cell column" style="background-color:#323A68;">
+                <div class="grid-x grid-margin-x grid-margin-y row">
+                    <div class="large-8 large-offset-2 cell column" style="color: white;">
                         <h3 style="color: white;">Goals of the Zúme Project:</h3>
                         <p>Zúme means yeast in Greek. In Matthew 13:33 Jesus is quoted as saying, "The Kingdom of
                             Heaven is like a woman who took yeast and mixed it into a large amount of flour until it
@@ -59,12 +59,12 @@ Template Name: Zume About
         <div class="grid-container fluid">
             <div class="medium-8 medium-offset-2 cell">
 
-                <div class="grid-x row">
-                    <div class="large-4 cell column">
+                <div class="grid-x ">
+                    <div class="large-4 cell ">
                         <img src="/wp-content/themes/zume-project-multilingual/assets/images/pages/training2.png"
                              alt="Zúme in 10 sessions, 2 hours each, cost: free"  height="150" />
                     </div>
-                    <div class="large-8 cell column">
+                    <div class="large-8 cell ">
                         <h3>Zúme Training consists of nine (two-hour) Basic Sessions and includes:</h3>
                         <ul>
                             <li>Video and Audio to help your group understand basic principles of multiplying
@@ -80,13 +80,13 @@ Template Name: Zume About
                 </div>
 
 
-                <div class="grid-x grid-margin-x grid-margin-y">
-                    <div class="medium-4 cell">
+                <div class="grid-x grid-margin-x grid-margin-y row">
+                    <div class="medium-4 cell column">
                         <img src="/wp-content/themes/zume-project-multilingual/assets/images/pages/getstarted.png"
                              alt=""
                              width="230" height="150"/>
                     </div>
-                    <div class="medium-8 cell">
+                    <div class="medium-8 cell column">
                         <h3>How to get started:</h3>
                         <ul style="margin-bottom: 1.8rem;">
                             <li>If you haven't created a login yet, please do so.</li>
@@ -99,12 +99,12 @@ Template Name: Zume About
                     </div>
                 </div>
 
-                <div class="grid-x grid-margin-x grid-margin-y">
-                    <div class="medium-4 cell">
+                <div class="grid-x grid-margin-x grid-margin-y row">
+                    <div class="medium-4 cell column">
                         <img src="/wp-content/themes/zume-project-multilingual/assets/images/pages/guidebook.png" alt=""
                              width="230" height="150"/>
                     </div>
-                    <div class="medium-8 cell">
+                    <div class="medium-8 cell column">
                         <h3>Optional prep for your first meeting:</h3>
                         <ul>
                             <li>Download the Zúme Guidebook.</li>
