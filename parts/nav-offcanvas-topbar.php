bbf0dc11a3a515e5c6cc24d3b579bb591c1ebd63
corsac <corsac@hushmail.com>
2017-12-12 11:41:18 +0100

    Improve menu navigation

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -16,23 +16,30 @@ if ( zume_has_polylang() ) {
      and a topbar for medium-up -->
 
 <div class="top-bar" id="top-bar-menu">
-    <div class="top-bar-left float-left">
+    <div class="show-for-large" style="display: flex; flex-direction: row; justify-content: space-around; width:100%">
+        <div class="menu-item show-for-large">
+            <a href="<?php echo is_user_logged_in() ? "/dashboard" : esc_attr( home_url() ) ?>">
+               <img src="<?php echo esc_attr( get_stylesheet_directory_uri() ); ?>/assets/images/zume-logo-white.png" class="zume-logo-in-top-bar">
+            </a>
+        </div>
+        <div class="menu-item show-for-large">
+            <?php zume_top_nav(); ?>
+        </div>
+        <div class="menu-item show-for-large"><?php pll_the_languages( array( 'dropdown' => 1 ) ) ?></div>
+    </div>
+
+
+    <div class=" show-for-small hide-for-large">
         <ul class="menu">
             <li class="zume-logo-in-top-bar"><a href="<?php if (is_user_logged_in()) { echo '/dashboard';
-} else { echo esc_attr( home_url() ); } ?>">
-               <img src="<?php echo esc_attr( get_stylesheet_directory_uri() ); ?>/assets/images/zume-logo-white.png" >
-            </a></li>
+                } else { echo esc_attr( home_url() ); } ?>">
+                    <img src="<?php echo esc_attr( get_stylesheet_directory_uri() ); ?>/assets/images/zume-logo-white.png" >
+                </a></li>
         </ul>
     </div>
-    <div class="top-bar-right float-right show-for-large">
-        <span style="float:right"><?php pll_the_languages( array( 'dropdown' => 1 ) ) ?></span>
-        <?php zume_top_nav(); ?>
-
-    </div>
-
-    <div class="top-bar-right float-right show-for-small hide-for-large ">
+    <div class="=show-for-small hide-for-large ">
         <ul class="menu float-right">
-            <!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
+             <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
             <li><a data-toggle="off-canvas"><?php esc_html_e( 'Menu', 'jointswp' ); ?></a></li>
         </ul>
     </div>
