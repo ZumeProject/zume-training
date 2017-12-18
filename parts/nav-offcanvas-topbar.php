bd50563193a5e4aabd5f7a1ed292cc70a405b1aa
Chris Chasm <chris@chasm.solutions>
2017-12-18 11:06:52 -0700

    Fixed language list for small screens
    https://github.com/ChasmSolutions/zume-project-multilingual/issues/12

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -16,8 +16,11 @@ if ( zume_has_polylang() ) {
      and a topbar for medium-up -->
 
 <div class="top-bar" id="top-bar-menu">
-    <div class="show-for-large" style="display: flex; flex-direction: row; justify-content: space-around; width:100%">
-        <div class="menu-item show-for-large">
+
+    <div class="" style="display: flex; flex-direction: row; justify-content: space-around; width:100%">
+
+        <!-- Show for large -->
+        <div class="menu-item">
             <a href="<?php echo is_user_logged_in() ? esc_url( zume_dashboard_url() ) : esc_attr( home_url() ); ?>">
                <img src="<?php echo esc_attr( zume_images_uri() ); ?>zume-logo-white.png" class="zume-logo-in-top-bar">
             </a>
@@ -25,23 +28,22 @@ if ( zume_has_polylang() ) {
         <div class="menu-item show-for-large">
             <?php zume_top_nav(); ?>
         </div>
-        <div class="menu-item show-for-large"><?php pll_the_languages( array( 'dropdown' => 1 ) ) ?></div>
+        <!-- End show for large -->
+
+        <!-- Show for all screens -->
+        <div class="menu-item"><?php pll_the_languages( array( 'dropdown' => 1 ) ) ?></div>
+        <!-- End show for all -->
+
+        <!-- Show for small/med -->
+        <div class="show-for-small hide-for-large">
+            <ul class="menu float-right">
+                <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+                <li><a data-toggle="off-canvas"><?php esc_html_e( 'Menu', 'zume' ); ?></a></li>
+            </ul>
+        </div>
+        <!-- End show for small/med -->
+
     </div>
 
-
-    <div class=" show-for-small hide-for-large">
-        <ul class="menu">
-            <li class="zume-logo-in-top-bar"><a href="<?php if (is_user_logged_in()) { echo esc_url( zume_dashboard_url() );
-} else { echo esc_attr( home_url() ); } ?>">
-                    <img src="<?php echo esc_attr( zume_images_uri() ); ?>zume-logo-white.png" >
-                </a></li>
-        </ul>
-    </div>
-    <div class="=show-for-small hide-for-large ">
-        <ul class="menu float-right">
-             <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
-            <li><a data-toggle="off-canvas"><?php esc_html_e( 'Menu', 'zume' ); ?></a></li>
-        </ul>
-    </div>
 </div>
 
