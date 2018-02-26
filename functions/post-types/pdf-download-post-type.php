ae6f8db779b36515cd6a4c49fe51bb6240ce629e
Chris Chasm <chris@chasm.solutions>
2018-02-26 15:13:26 -0700

    Spacing

--- functions/post-types/pdf-download-post-type.php
+++ functions/post-types/pdf-download-post-type.php
@@ -260,7 +260,7 @@ class Zume_PDF_Download_Post_Type
                 strtolower( $this->singular ),
                 // translators: Publish box date format, see http://php.net/date
                 '<strong>' . date_i18n( __( 'M j, Y @ G:i' ),
-                    strtotime( $post->post_date ) ) . '</strong>',
+                strtotime( $post->post_date ) ) . '</strong>',
                 '<a target="_blank" href="' . esc_url( get_permalink( $post->ID ) ) . '">',
                 '</a>'
             ),
