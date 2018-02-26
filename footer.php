cfc811f8c95d34e879ae38fb3e20641ff2a471f0
Chris Chasm <chris@chasm.solutions>
2018-02-26 11:51:44 -0700

    Opacity change on foot, added padding, centered

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
 <footer class="footer" role="contentinfo">
     <div id="inner-footer" class="grid-x">
         <div class="medium-1 cell"></div>
-        <div class="medium-10 cell">
-            <p class="source-org copyright">
+        <div class="medium-10 cell center">
+
+            <p class="source-org copyright hide-for-small-only" style="padding-top:50px; opacity:.3;">
                 <span>
-                    <a href="https://www.facebook.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images//facebook-square.svg" style="height: 23px" title="Facebook"></a>
+                    <a href="https://www.facebook.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images/facebook-square.svg" style="height: 23px" title="Facebook"></a>
                     <a href="https://twitter.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images/twitter-square.svg" style="height: 23px" title="Twitter"></a>
                 </span> &copy; <?php echo esc_html( date( 'Y' ) ); ?> Zúme Project
             </p>
