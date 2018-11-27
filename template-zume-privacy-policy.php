488873e6aa35a085ccda1a8cf2daf1d076743995
Chris Chasm <chris@chasm.solutions>
2018-11-27 10:16:30 -0700

    Privacy policy added

--- template-zume-privacy-policy.php
+++ template-zume-privacy-policy.php
@@ -29,7 +29,7 @@ Template Name: Zume Privacy Policy
                         <!-- Item -->
                         <div class="cell">
                             <hr>
-                            <?php esc_html_e( 'This privacy policy has been compiled to better serve those who are concerned with how their \'Personally Identifiable Information\' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.', 'zume' ) ?>
+                            <?php esc_html_e( 'This privacy policy has been compiled to better serve those who are concerned with how their "Personally Identifiable Information" (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.', 'zume' ) ?>
                         </div>
 
                         <!-- Item -->
@@ -49,7 +49,7 @@ Template Name: Zume Privacy Policy
                         <!-- Item -->
                         <div class="cell">
                             <hr>
-                            <h3 class="secondary"><?php esc_html_e( 'What permissions do the social sign-on logins ask for?', 'zume' ) ?></h3>
+                            <h3 class="secondary"><?php esc_html_e( 'What personal information do we collect from the people through our website?', 'zume' ) ?></h3>
                             <ul>
                                 <li>
                                     <?php esc_html_e( 'Information in the Basic Social Profile (if used) and email.', 'zume' ) ?>
@@ -92,6 +92,65 @@ Template Name: Zume Privacy Policy
                                     <?php esc_html_e( 'We email occasional reminders and encouragements depending on your progress through the training.', 'zume' ) ?>
                                 </li>
                             </ul>
+                        </div>
+
+                        <!-- Item -->
+                        <div class="cell">
+                            <hr>
+                            <h3 class="secondary"><?php esc_html_e( 'How do we protect your information?', 'zume' ) ?></h3>
+                            <p>
+                                <?php esc_html_e( 'While we use encryption to protect sensitive information transmitted online, we also protect your information offline. Only team members who need the information to perform a specific job (for example, web administrator or customer service) are granted access to personally identifiable information.', 'zume' ) ?>
+                            </p>
+                            <p>
+                                <?php esc_html_e( 'Your personal information is contained behind secured networks and is only accessible by a limited number of persons who have special access rights to such systems, and are required to keep the information confidential. In addition, all sensitive/credit information you supply is encrypted via Secure Socket Layer (SSL) technology.', 'zume' ) ?>
+                            </p>
+                            <p>
+                                <?php esc_html_e( 'We implement a variety of security measures when a user submits, or accesses their information to maintain the safety of your personal information.', 'zume' ) ?>
+                            </p>
+                        </div>
+
+                        <!-- Item -->
+                        <div class="cell">
+                            <hr>
+                            <h3 class="secondary"><?php esc_html_e( 'Do we use "cookies"?', 'zume' ) ?></h3>
+                            <p>
+                                <?php esc_html_e( 'Any use of Cookies - or of other tracking tools - by this Application or by the owners of third party services used by this Application, unless stated otherwise, serves to identify Users and remember their preferences, for the sole purpose of providing the service required by the User.', 'zume' ) ?>
+                            </p>
+                            <p>
+                                <?php esc_html_e( 'Personal Data collected: name, email.', 'zume' ) ?>
+                            </p>
+                        </div>
+
+                        <!-- Item -->
+                        <div class="cell">
+                            <hr>
+                            <h3 class="secondary"><?php esc_html_e( 'Your Access to and Control Over Information.', 'zume' ) ?></h3>
+                            <p>
+                                <?php esc_html_e( 'You may opt out of any future contact from us at any time. You can do the following at any time by contacting us via our contact email address:', 'zume' ) ?>
+                            </p>
+                            <p>
+                                <?php esc_html_e( 'See what data we have about you, if any.', 'zume' ) ?>
+                            </p>
+                            <ul>
+                                <li>
+                                    <?php esc_html_e( 'Change/correct any data we have about you.', 'zume' ) ?>
+                                </li>
+                                <li>
+                                    <?php esc_html_e( 'Have us delete any data we have about you.', 'zume' ) ?>
+                                </li>
+                                <li>
+                                    <?php esc_html_e( 'Express any concern you have about our use of your data.', 'zume' ) ?>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Item -->
+                        <div class="cell">
+                            <hr>
+                            <h3 class="secondary"><?php esc_html_e( 'Updates', 'zume' ) ?></h3>
+                            <p>
+                                <?php esc_html_e( 'Our Privacy Policy may change from time to time and all updates will be posted on this page.', 'zume' ) ?>
+                            </p>
                         </div>
 
 
