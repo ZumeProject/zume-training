9e5defcf833016f88b567a48b56d930c844e1694
Chris Chasm <chris@chasm.solutions>
2018-11-28 06:56:04 -0700

    Removed tracking from admin area for logrocket

--- functions/log-rocket-tracking.php
+++ functions/log-rocket-tracking.php
@@ -7,12 +7,14 @@
 
 function zume_add_logrocket_to_head() {
 $user = wp_get_current_user();
+if ( ! is_admin() ) :
 ?>
 <script src="https://cdn.logrocket.io/LogRocket.min.js" crossorigin="anonymous"></script>
 <script>window.LogRocket && window.LogRocket.init('mkpicf/zumeproject');</script>
 <?php if ( ! empty( $user ) && is_user_logged_in() ) : ?>
 <script>LogRocket.identify('<?php echo $user->ID ?>',{name:'<?php echo $user->display_name ?? '' ?>',email:'<?php echo $user->user_email ?? ''  ?>'});</script>
 <?php
+endif;
 endif;
 }
 add_action( 'wp_head', 'zume_add_logrocket_to_head' );
