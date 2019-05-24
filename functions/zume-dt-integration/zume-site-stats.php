3b8d9e6aeb1c04ec773bad80ab0c77874f09ca2b
Chris Chasm <chris@chasm.solutions>
2019-05-24 08:59:27 -0600

    Updated for training reporting

--- functions/zume-dt-integration/zume-site-stats.php
+++ functions/zume-dt-integration/zume-site-stats.php
@@ -62,7 +62,18 @@ class Zume_Site_Stats
 
         // Registered people
         $hero_stats['registered_people'] = self::query_registered_people();
-        $hero_stats['total_languages'] = count( pll_languages_list() );
+        // languages
+        $language = 0;
+        $dir_scan = scandir( get_template_directory() . '/translations/' );
+        if ( ! empty( $dir_scan ) ) {
+            foreach ( $dir_scan as $file ) {
+                if (  preg_match( '/.po/', $file ) ) {
+                    $language++;
+                }
+            }
+        }
+        $hero_stats['total_languages'] = $language;
+
         $countries = [];
 
         // Loop group details
