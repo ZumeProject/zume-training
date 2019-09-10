fd503d0104b21591b913ffba77956986bfcc0e8c
Chris Chasm <chris@chasm.solutions>
2019-09-10 16:19:05 -0600

    Updated gulp & package lock Added tools.scss Added template for person of peace

--- template-page-person-of-peace.php
+++ template-page-person-of-peace.php
@@ -1,156 +1,112 @@
 <?php
 /*
-Template Name: Zume POP
+Template Name: Tool - Person of Peace
 */
+
+/**
+ * Begin page template for Zume Overview
+ */
 get_header();
+
 ?>
 
-    <div id="content" class="grid-x grid-padding-x">
-        <div class="cell">
-            <div id="inner-content">
+    <div id="content">
 
-                <!-- Challenge -->
-                <div class="grid-x grid-margin-x grid-margin-y vertical-padding" style="max-width:100%; margin:0; background:white; padding:17px">
-                    <div class="medium-2 small-1 cell"></div>
-                    <div class="medium-8 small-10 cell center">
-                        4 of 30
-                        <h1 style="margin-bottom:0px;color: #323a68;">
-                            <strong><?php the_title(); ?></strong>
-                        </h1>
+        <div id="inner-content" class="grid-x grid-padding-x">
+            <div class="cell medium-1 hide-for-small-only"></div>
+
+            <div id="concepts-tools" class="cell auto" role="main">
+
+                <div class="grid-x grid-padding-x">
+
+                    <!-- Side Navigation -->
+                    <div class="cell large-4 show-for-large" style="max-width:300px">
+                    <?php get_template_part( 'parts/tools', 'menu' ); ?>
                     </div>
-                    <div class="medium-2 small-1 cell"></div>
+
+                    <div class="cell auto">
+                        <div class="grid-x grid-padding-y">
+                            <div class="cell center">
+                                <img src="https://zume-project.com/wp-content/themes/zume-project-multilingual/assets/images/overview/4.1.png" />
+                                <h1>Person of Peace</h1>
+                            </div>
+                            <div class="cell">
+                                A Person of Peace is:
+                                <ul>
+                                    <li>Someone who is OPEN to hearing Your Story, God's Story, and the Good News of Jesus.</li>
+                                    <li>Someone who is HOSPITABLE and WELCOMES you into their home or their workplace or to join events with family and friends.</li>
+                                    <li>Someone who KNOWS OTHERS (or is KNOWN BY OTHERS) and who is excited to draw together a small group or even a crowd.</li>
+                                    <li>Someone who is FAITHFUL and SHARES what they learn with others - even after you're gone.</li>
+                                </ul>
+                                A Person of Peace can help rapidly reproduce disciple-making even in a place where followers of Jesus are few and far between. When you want to make disciples in a place where not many - or maybe even any - exist, then looking for a Person of Peace might be the most important thing you do.
+
+                            </div>
+                            <div class="cell center">
+                                <h2>Watch the Concept Video</h2>
+                                <!-- 18 -->
+                                <iframe style="border: 1px solid lightgrey;"  src="<?php echo esc_url( Zume_Course::get_video_by_key( '18' ) ) ?>" width="560" height="315"
+                                        frameborder="1"
+                                        webkitallowfullscreen mozallowfullscreen allowfullscreen>
+                                </iframe>
+                            </div>
+
+                            <div class="cell host">
+                                <hr>
+                                <h2 class="center">You've heard the concept. We challenge you to live it, share it, and train others!</h2><br>
+                                <table id="host-table" class="unstriped hover">
+                                    <tbody>
+                                    <tr>
+                                        <td><span class="letter active">H</span></td>
+                                        <td><span class="label-big">Heard</span></td>
+                                        <td><span class="label-big success">&#10004</span></td>
+                                        <td>"Heard" means you have become aware. Most Christians have not heard the idea shared above, but now you have.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="letter">O</span></td>
+                                        <td><span class="label-big">Obeyed</span></td>
+                                        <td><span class="label-big alert">&#10008</span></td>
+                                        <td>"Obeyed" means you have taken personal action. Obeying with tools might look like beginning to use them with others, while obeying with concepts might look like changing thinking or priorities.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="letter">S</span></td>
+                                        <td><span class="label-big">Shared</span></td>
+                                        <td><span class="label-big alert">&#10008</span></td>
+                                        <td>"Shared" means you have shared the tool with someone else. This step is essential to truly understanding the concept or tool and preparing you to train others.</td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="letter">T</span></td>
+                                        <td><span class="label-big">Trained</span></td>
+                                        <td><span class="label-big alert">&#10008</span></td>
+                                        <td>"Trained" means you have trained someone else to hear, obey, and share the tool or concept with the hope that they would train someone else.</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <h2 class="center"><a href="/login">Login and track your progress!</a></h2>
+                            </div>
+
+                            <div class="cell center">
+                                <hr>
+                                <p class="p-large">Zúme Training is an on-line and in-life learning experience designed for small groups who follow Jesus to learn how to obey His Great Commission and make disciples who multiply.</p>
+                                <button class="button-jumbo-action">Register for Free Training</button>
+                            </div>
+                        </div>
+
+                    </div> <!-- main column-->
+
+                    <div class="cell large-3 hide-for-large">
+                        <br><br>
+                    <?php get_template_part( 'parts/tools', 'menu' ); ?>
+                    </div>
+
                 </div>
 
-                <div class="grid-x ">
-                    <div class="large-2 cell"></div>
+            </div> <!-- end #main -->
+            <div class="cell medium-1 hide-for-small-only"></div>
 
-                    <div class="large-8 small-12 cell">
+        </div> <!-- end #inner-content -->
 
-                        <section id="session-p-2" role="tabpanel" aria-labelledby="session-h-2" class="body current" aria-hidden="false" style="display: block;">
+    </div><!-- end #content -->
 
-                            <!-- Activity Block  -->
-                            <div class="grid-x grid-margin-x grid-margin-y">
+<?php
 
-                                <div class="large-12 cell activity-description-no-border">
-                                    Disciple-making can be rapidly advanced by finding a person of peace, even in a place where followers of Jesus are few and far between. How do you know when you have found a person of peace and what do you when you find them?                </div>
-                            </div> <!-- grid-x -->
-
-                            <!-- Video block -->
-                            <div class="grid-x grid-margin-x grid-margin-y">
-
-                                <div class="small-12 small-centered cell video-section">
-
-                                    <!-- 18 -->
-                                    <div class="widescreen responsive-embed"><iframe style="border: 1px solid lightgrey;" src="https://player.vimeo.com/video/248149796" width="560" height="315" frameborder="1" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="">
-                                        </iframe></div>
-
-
-                                </div>
-                            </div> <!-- grid-x -->
-
-                            <!-- Share Section -->
-                            <div class="grid-x grid-margin-x grid-margin-y">
-                                <div class="small-12 small-centered cell center">
-                                    <h3>Like this concept?</h3>
-                                    <p style="font-size: 1.5rem;">
-                                        Person of Peace is just one concept in the larger Zume training for making multiplying disciples. Sign up today and get started.
-                                    </p>
-                                </div>
-<!--                                <div class="small-12 small-centered cell center">-->
-<!--                                    <p><button class="button large" style="width:300px;">Share it!</button></p>-->
-<!--                                    <p><button class="button large" style="width:300px;">Join us!</button></p>-->
-<!--                                    <p><button class="button large" style="width:300px;">Watch more videos</button></p>-->
-<!--                                </div>-->
-                            </div>
-                            <div class="grid-x grid-margin-x grid-margin-y">
-                                <div class="small-4 cell center" style="background-color: #0A246A;color:white; padding:1em; font-size: 2em;">
-                                    <i class="fi-like" style="color:white;font-size:3em;"></i><br>
-                                    Share this video with a friend.
-                                </div>
-                                <div class="small-4 cell center" style="background-color: #0A246A;color:white; padding:1em; font-size: 2em;">
-                                    <i class="fi-compass" style="color:white;font-size:3em;"></i><br>
-                                    Get the entire training.
-                                </div>
-                                <div class="small-4 cell center" style="background-color: #0A246A;color:white; padding:1em; font-size: 2em;">
-                                    <i class="fi-thumbnails" style="color:white;font-size:3em;"></i><br>
-                                    See more related videos.
-                                </div>
-                            </div>
-
-
-                            <div class="grid-x grid-margin-x grid-margin-y">
-                                <div class="large-12 cell activity-description-no-border center">
-                                    <h3 class="center">Video Transcript</h3>
-                                </div>
-                                <div class="large-12 cell ">
-                                    A Person of Peace can help rapidly reproduce disciple-making even in a place where followers of Jesus are few and far between.
-                                        <br><br>
-                                    When Jesus sent out His disciples to new territory to make disciples, He gave them a simple but strategic command.
-                                        <br><br>
-                                    Jesus said -- Carry no money belt, no bag, no shoes; greet no one on the way. Whatever house you enter, first say, ‘Peace be to this house.’ If a man of peace is there, your peace will rest on him; but if not, it will return to you. Stay in that same house, eating and drinking what they give you; for the laborer is worthy of his wages. Do not keep moving from house to house.
-                                        <br><br>
-                                    But what does all that mean?<br>
-                                        <br><br>
-                                    When we think of making disciples, our first thought might be -- We better get our finances in order, pick an obvious target, and have a clear plan of action. If Jesus said - “Go”, we better “go” and keep on going! Tell Everyone! Everywhere!! All the time!!!
-                                        <br><br>
-                                    But Jesus, in His instructions, seemed a lot less worried about finances and fervor, and a lot more concerned about focus.
-                                        <br><br>
-                                    Jesus wanted His disciples looking for - and investing in - a Person of Peace.
-                                    When you want to make disciples in a place where not many - or maybe even any - exist, then looking for a Person of Peace might be the most important thing you do.
-                                    A Person of Peace is:
-                                        <br><br>
-                                    Someone who is OPEN to hearing Your Story, God’s Story and the Good News of Jesus.
-                                        <br><br>
-                                    Someone who is HOSPITABLE and WELCOMES you into their home or their workplace or to join events with family and friends.
-                                        <br><br>
-                                    Someone who KNOWS OTHERS (or is KNOWN BY OTHERS) and who is excited to draw together a small group or even a crowd.
-                                        <br><br>
-                                    Someone who is FAITHFUL and SHARES what they learn with others - even after you’re gone.
-                                        <br><br>
-                                    In the Bible, we learn of Jesus and His followers meeting Persons of Peace that are a little - unexpected.
-                                        <br><br>
-                                    In the region of the Gadarenes, Jesus met a demon-possessed man who lived isolated and in chains. We would never think of Him as a Person of Peace, but He was OPEN to hear from Jesus. He was HOSPITABLE and WELCOMED Jesus to where He lived. He was WELL KNOWN and could easily draw a crowd - even if only for his outrageous behavior. And Jesus found that he was FAITHFUL and SHARED what Jesus meant to him with his family, his community and his whole country. In fact, when Jesus came back to that area, a great crowd gathered, excited to see the Man they had heard so much about.
-                                    In Samaria, Jesus met a woman at a well. She was OPEN to Jesus, willing to be HOSPITABLE and answer His request for a drink. We learn she had five husbands and was living with still another man, and so in a small town, she was sure to be KNOWN BY OTHERS. And after Jesus spoke to her, she was FAITHFUL and SHARED - so much and so quickly that the entire town asked Jesus to stay and share with them, too. And He did.
-                                        <br><br>
-                                    So, if a Person of Peace can live almost anywhere, do almost anything, and be almost anyone we know or meet - how do we find one?
-                                        <br><br>
-                                    Here are three simple ways --
-                                        <br><br>
-                                    We ASK for recommendations from people in the community - Who’s someone that’s trusted here? Is there someone in this place who thinks of others before themselves? If we hear the same name again and again - we try to meet them, share spiritual ideas, and see if they’re open to hear and share.
-                                        <br><br>
-                                    We OFFER to pray for someone while Prayer Walking, or at work, or at the game - wherever there’s an opportunity - and then turn that prayer into a spiritual conversation.
-                                        <br><br>
-                                    We INTRODUCE spiritual ideas into every conversation to see if God is working in a person’s life. If they are open and willing, then we ask if they would be willing to gather a group to discuss even more.
-                                        <br><br>
-                                    ASK for recommendations, OFFER to pray, INTRODUCE spiritual ideas. These are all ways we can begin the process of finding a Person of Peace.
-                                        <br><br>
-                                    And no matter how we find them, remember Jesus said a Person of Peace is someone we should be spending most of our disciple-making time with.
-                                        <br><br>
-                                    It’s easy to think that the most “fair” use of our time is to give away a little bit of ourselves to everyone, equally. But Jesus said - and showed - that He doesn’t want us to be shallow with everyone but to give deeply to a few.
-                                        <br><br>
-                                    Jesus often attracted crowds, but the Bible tells us again and again that Jesus would draw away from those crowds to spend most of His time with just twelve of His closest followers.
-                                        <br><br>
-                                    There were a number of times, where Jesus would invest even more time with a smaller group of just three.
-                                        <br><br>
-                                    If Jesus, who had much more power, much more energy, much more authority, discipline, wisdom, knowledge, understanding and compassion chose to spend His time investing deeply in just a few and told His own disciples to do the same, doesn’t it make sense that we should follow and share His perfect pattern? A Person of Peace.
-                                        <br><br>
-                                    They’re not easy to find - maybe one in a thousand. But like a hidden treasure that’s worth the search, their value in growing God’s family can’t be measured.
-                            </div>
-                            </div>
-
-                    </section>
-
-                    </div>
-                    <div class="large-2 cell"></div>
-                </div>
-
-            </div> <!-- end #inner-content -->
-
-
-        </div> <!-- cell -->
-    </div> <!-- content -->
-
-    <?php
-
-    get_footer();
-    ?>
+get_footer();
