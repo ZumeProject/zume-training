826ff2112f15197c1deb2001226ba04afa83d97c
Chris Chasm <chris@chasm.solutions>
2019-09-11 12:07:09 -0600

    producer

---
+++ template-page-producer-consumer.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Template Name: Tool - Producer vs Consumer
+*/
+
+get_header();
+
+?>
+
+    <div id="content">
+
+        <div id="inner-content" class="grid-x grid-padding-x">
+
+            <div class="cell medium-1 hide-for-small-only"></div> <!-- edge padding -->
+
+            <div id="concepts-tools" class="cell auto" role="main">
+
+                <div id="content-section" class="grid-x grid-padding-x">
+
+                    <!-- Show at side for larger screens -->
+                    <div class="cell large-4 show-for-large" style="max-width:300px">
+                        <?php get_template_part( 'parts/tools', 'menu' ); ?>
+                    </div>
+                    <!-- end menu-->
+
+                    <!-- Begin main content -->
+                    <div class="cell auto">
+                        <div class="grid-x grid-padding-y">
+                            <!-- Page Content -->
+
+                            <div class="cell center">
+                                <img src="https://zume-project.com/wp-content/themes/zume-project-multilingual/assets/images/overview/4.1.png" />
+                                <h1>Producers vs. Consumers</h1>
+                            </div>
+                            <div class="cell">
+
+                                    If we want to make disciples who multiply – spiritual producers and not just consumers – then we need to learn and share four main ways God makes everyday followers more like Jesus:
+                                <ul>
+                                    <li>Prayer</li>
+                                    <li>Scripture</li>
+                                    <li>Body Life</li>
+                                    <li>Persecution and Suffering</li>
+                                </ul>
+                                <h2>Watch the Concept Video</h2>
+                                <!-- 18 -->
+                                <div class="widescreen responsive-embed">
+                                    <iframe style="border: 1px solid lightgrey;" src="https://player.vimeo.com/video/247063338" width="560" height="315" frameborder="1" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="">
+                                    </iframe>
+                                </div>
+                                In this video, we’ll talk about how we can help a follower of Jesus become a PRODUCER in
+                                God’s kingdom instead of simply a CONSUMER.
+                                In His perfect plan, God created us to live in balance - to PRODUCE and to CONSUME, to
+                                CREATE and to USE UP, to POUR OUT and to be FILLED so we can POUR OUT again.
+                                But, in our broken world, people have rejected God’s plan, and many spend their energy living out
+                                just part of God’s perfect equation.
+                                They LEARN but they don’t share. They are are FILLED UP but they never pour out. They
+                                CONSUME but they don’t produce.<br><br>
+                                If we’re going to make disciples who multiply, then we need to share with them how they can be
+                                producers and not just consumers.<br><br>
+                                <strong>Scripture</strong><br><br>
+                                Here’s how — God uses His Written Word - which we call Scripture or The Bible - to grow us
+                                spiritually.
+                                Every disciple needs to be equipped to learn, interpret and apply Scripture.
+                                Over thousands of years and through many different authors, God spoke His word into the hearts
+                                of faithful men who captured and shared what they heard.<br><br>
+                                The Scriptures teach us God’s story, His plans, His heart, His ways.<br><br>
+                                In an earlier session, you learned two simple tools - SOAPS Bible Study and Accountability
+                                Groups. In an upcoming session, you’ll learn one more simple tool - 3/3rds Groups.
+                                These three tools work together to help equip new followers to learn, interpret and apply God’s
+                                Written Word.<br><br>
+                                They will learn not to be just hearers of God’s word but doers and sharers, also.
+                                God also uses His Spoken Word - which we can discern through Prayer - to grow us spiritually.<br><br>
+                                <strong>Prayer</strong><br><br>
+                                Prayer is speaking and listening to God. Prayer helps us know God more intimately and
+                                understand His heart, His will and His ways.<br><br>
+                                Prayer helps us minister and serve others, helps us teach and share in specific ways that helps
+                                individuals or a group know God better.<br><br>
+                                Two simple tools - Prayer Walking and The Prayer Cycle help followers develop a personal prayer
+                                life and learn to pray in ways that serve others.<br><br>
+                                These tools help develop a habit of praying without ceasing and learning to see the world from a
+                                spiritual perspective instead of only relying on what we can visibly see.<br><br>
+                                When used consistently, they help a follower of Jesus, increase their capacity for prayer and
+                                enhance their ability to hear from God and share what they hear.<br><br>
+                                <strong>Body</strong><br><br>
+                                God uses His Body of Believers - which we call The Church or the Followers of Jesus - to grow us
+                                spiritually.<br><br>
+                                As the gathering of believers, we are connected. God’s Word says that in Jesus - we are many
+                                parts of one body, and we all belong to each other. In other words, we’re not just connected to
+                                God - we’re connected with each other.<br><br>
+                                God says to submit to one another. God says to serve one another.<br><br>
+                                Each of us has different strengths and each has weaknesses. God expects us to use our
+                                strengths to help others who may be weak. And He expects us to allow others to help us in our
+                                weakness using the strengths He has given them.<br><br>
+                                God’s Word says God has given each of you some special abilities; be sure to use them to help
+                                each other, passing on to others God’s many kinds of blessings.<br><br>
+                                Simple tools like 3/3rds Groups, Accountability Groups and Peer Mentoring help us encourage
+                                one another to love and good works by not only helping us to obey what God tells us to do but
+                                also helping us find ways to share what we learn with others.<br><br>
+                                <strong>Suffering</strong><br><br>
+                                God also uses Persecution and Suffering - sacrifice and loss that we suffer on behalf of Jesus - to
+                                grow us spiritually.<br><br>
+                                When people oppress and hurt us because we love and obey Jesus, or when bad things happen
+                                even though we love and obey Jesus, God uses those persecutions and sufferings to refine our
+                                character and make us more like Jesus.<br><br>
+                                He develops our character, strengthens and purifies our faith, equips for ministry and allows us to
+                                serve others who are suffering in a special way - all while making Himself known more clearly to
+                                everyone who watches us and knows our pain.<br><br>
+                                God tells us that as followers of Jesus we should expect to be persecuted.
+                                Jesus said - God will bless you when people insult you, mistreat you, and tell all kinds of evil lies
+                                about you because of me. Be happy and excited! You will have a great reward in heaven. People
+                                did these same things to the prophets who lived long ago.<br><br>
+                                Simple tools like 3/3rds Groups and Accountability Groups give followers of Jesus an opportunity
+                                to share the persecutions and sufferings they experience.<br><br>
+                                These groups give you a chance to teach disciples that God’s word says we should expect hard
+                                times and to equip them in how to respond well by trusting God’s love even when things go
+                                wrong.<br><br>
+                                Scripture. Prayer. Body Life. Persecution and Suffering.<br><br>
+                                These are all ways that God grows us to be more like His perfect Son, Jesus.
+                                Simple tools help us not to just be consumers of these good things that God has given us but to
+                                be producers and sharers as well.<br><br>
+
+                            </div>
+                            <div class="cell center">
+
+                            </div>
+                            <!-- End Page Content -->
+
+                            <!-- Track Progress Section-->
+                            <?php get_template_part( 'parts/tools', 'trackprogress' ); ?>
+
+                            <!-- Challenge to Register-->
+                            <?php get_template_part( 'parts/tools', 'register' ); ?>
+
+                        </div>
+
+                    </div> <!-- main column-->
+
+                    <!-- Show at bottom on smaller screens -->
+                    <div class="cell large-3 hide-for-large">
+                        <br><br>
+                        <?php get_template_part( 'parts/tools', 'menu' ); ?>
+                    </div>
+                    <!-- end menu-->
+
+                </div><!-- end conent-section-->
+
+            </div> <!-- end #main -->
+
+            <div class="cell medium-1 hide-for-small-only"></div><!-- edge padding -->
+
+        </div> <!-- end #inner-content -->
+
+    </div><!-- end #content -->
+
+<?php
+
+get_footer();
