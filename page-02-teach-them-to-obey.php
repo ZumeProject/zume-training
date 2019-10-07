0edfdd4c82c7d1d1112b4c45c708956cf368016a
Chris Chasm <chris@chasm.solutions>
2019-10-07 17:13:47 -0600

    Added page template

--- page-02-teach-them-to-obey.php
+++ page-02-teach-them-to-obey.php
@@ -2,3 +2,97 @@
 /*
 Template Name: 02 - Teach to Obey
 */
+get_header();
+if (have_posts()) :
+    while (have_posts()) : the_post();
+        ?>
+
+        <!-- Wrappers -->
+        <div id="content" class="grid-x grid-padding-x"><div  id="inner-content" class="cell">
+
+                <!------------------------------------------------------------------------------------------------>
+                <!-- Title section -->
+                <!------------------------------------------------------------------------------------------------>
+                <div class="grid-x grid-margin-x grid-margin-y vertical-padding">
+                    <div class="medium-2 small-1 cell"></div><!-- Side spacer -->
+
+                    <!-- Center column -->
+                    <div class="medium-8 small-10 cell center">
+
+                        <img src="<?php echo get_theme_file_uri() ?>/assets/images/zume_images/V5.1/1Waving1Not.svg" width="200px" />
+
+                        <h1>
+                            <strong><?php the_title(); ?></strong>
+                        </h1>
+                        <p>
+                            <a href="/dashboard"><?php echo esc_html__( 'This concept comes from the Zúme Training Course', 'zume' ) ?> - <?php echo esc_html__('Session', 'zume' ) ?> 5</a>.
+                        </p>
+                    </div>
+
+                    <div class="medium-2 small-1 cell"></div><!-- Side spacer -->
+                </div>
+
+
+                <!------------------------------------------------------------------------------------------------>
+                <!-- Unique page content section -->
+                <!------------------------------------------------------------------------------------------------>
+                <div class="grid-x ">
+                    <div class="large-2 cell"></div><!-- Side spacer -->
+
+                    <!-- Center column -->
+                    <div class="large-8 small-12 cell" id="training-content">
+
+                    </div>
+
+                    <div class="large-2 cell"></div><!-- Side spacer -->
+                </div> <!-- grid-x -->
+
+
+                <!------------------------------------------------------------------------------------------------>
+                <!-- Share section -->
+                <!------------------------------------------------------------------------------------------------>
+                <div class="grid-x ">
+                    <div class="large-2 cell"></div><!-- Side spacer -->
+
+                    <!-- Center column -->
+                    <div class="large-8 small-12 cell">
+
+                        <?php get_template_part( 'parts/p', 'share' ); ?>
+
+                    </div>
+                    <div class="large-2 cell"></div><!-- Side spacer -->
+                </div> <!-- grid-x -->
+
+
+                <!------------------------------------------------------------------------------------------------>
+                <!-- Transcription section -->
+                <!------------------------------------------------------------------------------------------------>
+                <div class="grid-x">
+                    <div class="large-2 cell"></div><!-- Side spacer -->
+
+                    <!-- Center column -->
+                    <div class="large-8 small-12 cell">
+                        <hr>
+                        <div class="grid-x grid-margin-x grid-margin-y">
+                            <div class="large-12 cell activity-description-no-border center">
+                                <h3 class="center"><?php echo esc_html__('Video Transcript', 'zume' ) ?></h3>
+                            </div>
+                            <div class="large-12 cell activity-description-no-border">
+
+                                <?php the_content(); ?>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="large-2 cell"></div><!-- Side spacer -->
+                </div> <!-- grid-x -->
+
+            </div> <!-- end #inner-content --></div> <!-- end #content -->
+
+    <?php
+    endwhile;
+endif;
+get_footer();
