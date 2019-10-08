a04d9da2a6fe027480457e67dc5e346aa48e3dc9
Chris Chasm <chris@chasm.solutions>
2019-10-07 18:00:15 -0600

    Added training dashboard

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
                 <span>
                     <a href="https://www.facebook.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images/facebook-square.svg" style="height: 23px" title="Facebook"></a>
                     <a href="https://twitter.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images/twitter-square.svg" style="height: 23px" title="Twitter"></a>
-                </span> &copy; <?php echo esc_html( date( 'Y' ) ); ?> Zúme Project
+                </span> &copy; <?php echo esc_html( date( 'Y' ) ); ?> Zúme
             </p>
             <br><br><br>
         </div>
---
+++ template-zume-training.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Template Name: Training
+*/
+
+get_header();
+
+?>
+
+<div id="content">
+    <div  id="inner-content" class="grid-x grid-margin-x grid-padding-x">
+        <div id="challenge" class="cell">
+            <div class="grid-x" id="test-eq">
+                <div class="large-1 cell"></div> <!-- side padding -->
+
+                <!-- Center Column -->
+                <div class="large-10 cell">
+
+                    <div class="callout">
+
+                    </div><!-- end #callout -->
+
+
+                </div> <!-- end center column -->
+
+                <div class="large-1 cell"></div> <!-- side padding -->
+            </div>
+        </div> <!-- end #challenge-->
+
+        <div id="course" class="cell">
+            <div class="grid-x" id="test-eq">
+                <div class="large-1 cell"></div> <!-- side padding -->
+
+
+                <div class="large-10 cell"><!-- Center Column -->
+
+                    <div class="callout">
+
+                        <div class="grid-x">
+                            <div class="cell medium-6">
+                                <h1>Session 1</h1>
+                                God Uses Ordinary People<br>
+                                Teach them to Obey<br>
+                                Spiritual Breathing<br>
+                                SOAPS Bible Reading<br>
+                                Accountability Groups<br>
+                            </div>
+                            <div class="cell medium-6">
+                                <h1>Session 2</h1>
+                                Consumers vs Producers<br>
+                                Prayer Cycle<br>
+                                List of 100<br>
+
+                            </div>
+                        </div>
+
+                    </div> <!-- end #callout -->
+
+                </div> <!-- end center column -->
+
+                <div class="large-1 cell"></div> <!-- side padding -->
+            </div>
+        </div> <!-- end #challenge-->
+    </div> <!-- end #inner-content -->
+</div> <!-- end #content -->
+
+<?php
+
+get_footer();
