2fe320e78e15b25db83f58befbbc18ccd5e77501
Chris Chasm <chris@chasm.solutions>
2019-11-11 13:09:41 -0700

    Training Cycle: Arranged content.

--- landing-22-training-cycle.php
+++ landing-22-training-cycle.php
@@ -50,15 +50,7 @@ if (have_posts()) :
                             <!-- Activity Block  -->
                             <div class="grid-x grid-margin-x grid-margin-y">
                                 <div class="cell content-large">
-
-                                    <p><?php esc_html_e( "Have you ever learned how to ride a bicycle? Have you ever helped someone else learn? If so, chances are you already know THE TRAINING CYCLE. It’s as easy as MODEL, ASSIST, WATCH and LEAVE. It works like this:", 'zume' ) ?></p>
-
-                                    <img src="<?php echo esc_url( zume_images_uri( 'temp/' ) ) ?>training-cycle.png" alt="Training Cycle" />
-
-                                    <p><?php esc_html_e("MODEL", 'zume' ) ?><?php esc_html_e( "Modeling is simply providing an example of a practice or tool. It is the briefest part of the training cycle. It usually only needs to be done once. It is simply creating an awareness that a practice or a tool exists and giving a general idea of what it looks like. Modeling repeatedly is not an effective way to equip someone. They need to be allowed to try the skill themselves. When a child sees someone riding a bicycle, that is the MODEL phase.", 'zume' ) ?></p>
-                                    <p><?php esc_html_e("ASSIST", 'zume' ) ?><?php esc_html_e( "Assisting is allowing the learner to practice the skill. This takes longer than the modeling phase. It requires “hand-holding” on the part of the mentor. The mentor needs to be directive and take an active role in coaching the learner. This phase does not last until the learner is fully competent, but merely until they understand the basics of the skill. If this phase is continued too long, then the learner will develop a dependence on the mentor and never advance to full competence. The end of the assist phase should be marked by the learner starting to model for others. When a parent is holding onto the bicycle while a child is learning to keep his balance, that is the ASSIST phase.", 'zume' ) ?></p>
-                                    <p><?php esc_html_e("WATCH", 'zume' ) ?><?php esc_html_e( "Watching is the longest phase. It involves more indirect contact with the learner. It seeks to develop full competence in all facets of a skill. It may be ten times or more as long as the first two phases combined. As the learner progresses in skill, the contact with the mentor may become less regular and more ad hoc . In this phase the learner gradually takes more responsibility and initiative in the performance of the skill. Typically in disciplemaking the mark of the end of this phase is when the learner has passed on the skill successfully to the fourth generation through those whom he or she is coaching. When a parent is observing a child ride a bicycle and ensuring they have adequate skills and knowledge to ride unsupervised, this is the WATCH phase.", 'zume' ) ?></p>
-                                    <p><?php esc_html_e("LEAVE", 'zume' ) ?><?php esc_html_e( "Leaving is a sort of graduation when the learner becomes a peer of the mentor. Periodic contact and peer mentoring may continue to take place if the learner and mentor are in the same network. When a parent releases a child to ride their bicycle completely unsupervised, that is the LEAVE phase.", 'zume' ) ?></p>
+                                    <p><?php esc_html_e( "Have you ever learned how to ride a bicycle? Have you ever helped someone else learn? If so, chances are you already know THE TRAINING CYCLE. It’s as easy as MODEL, ASSIST, WATCH and LEAVE.", 'zume' ) ?></p>
 
                                 </div>
                             </div> <!-- grid-x -->
@@ -82,6 +74,19 @@ if (have_posts()) :
 
                                 </div>
                             </div> <!-- grid-x -->
+
+                            <!-- Activity Block  -->
+                            <div class="grid-x grid-margin-x grid-margin-y">
+                                <div class="cell content-large">
+                                    <p><h3><?php esc_html_e( "The Training Cycle works like this:", 'zume' ) ?></h3></p>
+                                    <img src="<?php echo esc_url( zume_images_uri( 'temp/' ) ) ?>training-cycle.png" alt="Training Cycle" />
+                                    <p><strong><?php esc_html_e("MODEL", 'zume') ?></strong><br><?php esc_html_e( "Modeling is simply providing an example of a practice or tool. It is the briefest part of the training cycle. It usually only needs to be done once. It is simply creating an awareness that a practice or a tool exists and giving a general idea of what it looks like. Modeling repeatedly is not an effective way to equip someone. They need to be allowed to try the skill themselves. When a child sees someone riding a bicycle, that is the MODEL phase.", 'zume' ) ?></p>
+                                    <p><strong><?php esc_html_e("ASSIST", 'zume') ?></strong><br><?php esc_html_e( "Assisting is allowing the learner to practice the skill. This takes longer than the modeling phase. It requires “hand-holding” on the part of the mentor. The mentor needs to be directive and take an active role in coaching the learner. This phase does not last until the learner is fully competent, but merely until they understand the basics of the skill. If this phase is continued too long, then the learner will develop a dependence on the mentor and never advance to full competence. The end of the assist phase should be marked by the learner starting to model for others. When a parent is holding onto the bicycle while a child is learning to keep his balance, that is the ASSIST phase.", 'zume' ) ?></p>
+                                    <p><strong><?php esc_html_e("WATCH", 'zume') ?></strong><br><?php esc_html_e( "Watching is the longest phase. It involves more indirect contact with the learner. It seeks to develop full competence in all facets of a skill. It may be ten times or more as long as the first two phases combined. As the learner progresses in skill, the contact with the mentor may become less regular and more ad hoc . In this phase the learner gradually takes more responsibility and initiative in the performance of the skill. Typically in disciplemaking the mark of the end of this phase is when the learner has passed on the skill successfully to the fourth generation through those whom he or she is coaching. When a parent is observing a child ride a bicycle and ensuring they have adequate skills and knowledge to ride unsupervised, this is the WATCH phase.", 'zume' ) ?></p>
+                                    <p><strong><?php esc_html_e("LEAVE", 'zume') ?></strong><br><?php esc_html_e( "Leaving is a sort of graduation when the learner becomes a peer of the mentor. Periodic contact and peer mentoring may continue to take place if the learner and mentor are in the same network. When a parent releases a child to ride their bicycle completely unsupervised, that is the LEAVE phase.", 'zume' ) ?></p>
+                                </div>
+                            </div> <!-- grid-x -->
+
                             <!-- Activity Block  -->
                             <div class="grid-x grid-margin-x grid-margin-y">
                                 <div class="cell content-large center">
