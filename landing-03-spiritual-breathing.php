8ba64d2c1c37519b81ff281ff63cd0c930846a35
Chris Chasm <chris@chasm.solutions>
2019-12-16 17:39:01 -0700

    SEO #3

--- landing-03-spiritual-breathing.php
+++ landing-03-spiritual-breathing.php
@@ -52,8 +52,8 @@ if (have_posts()) :
                             <!-- Activity Block -->
                             <div class="grid-x grid-margin-x grid-margin-y">
                                 <div class="cell content-large">
-                                    <p><?php esc_html_e( "We breathe in. We breathe out. Life. Likewise, in God’s Kingdom, spiritual breathing is hearing and obeying God ... all day, every day.", 'zume' ) ?></p>
-                                    <p><?php esc_html_e( "In the Kingdom, we breathe IN when we HEAR from God. In the Kingdom we breathe OUT when we ACT on what we hear from God. We breathe OUT when we OBEY.", 'zume' ) ?></p>
+                                    <p><?php esc_html_e( "Spiritual breathing is hearing and obeying God ... all day, every day.", 'zume' ) ?></p>
+                                    <p><?php esc_html_e( "In the Kingdom, we breathe IN when we HEAR from God. In the Kingdom we breathe OUT when we ACT on what we hear from God. We breathe OUT when we OBEY. We breathe in. We breathe out. Life.", 'zume' ) ?></p>
                                     <p><?php esc_html_e( "The good news for every follower of Jesus is that when we breathe IN and HEAR from God and when we breathe OUT and OBEY what we hear and SHARE with others what we’ve heard - God will speak even more clearly.", 'zume' ) ?></p>
                                 </div>
                             </div>
