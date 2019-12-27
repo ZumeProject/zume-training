b9d9df045af91b0a7e8e12fd6cd096043fe19946
Chris Chasm <chris@chasm.solutions>
2019-12-27 10:26:46 -0700

    Updated nav

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -6,14 +6,14 @@
 
 ?>
 
-<div class="grid-x top-bar">
-    <div class="cell hide-for-small-only medium-1"></div>
+<div class="grid-x margin-horizontal-1 top-bar">
+
     <div class="cell small-3 medium-2" id="top-logo-div">
         <a href="<?php echo is_user_logged_in() ? esc_url( zume_dashboard_url() ) : esc_url( zume_home_url() ); ?>">
             <div class="zume-logo-in-top-bar"></div>
         </a>
     </div>
-    <div class="cell medium-5 show-for-medium center" id="top-full-menu-div-wrapper">
+    <div class="cell medium-5 hide-for-small show-for-large center" id="top-full-menu-div-wrapper">
         <div id="top-full-menu-div">
             <?php zume_top_nav(); ?>
         </div>
@@ -21,8 +21,8 @@
     <div class="cell small-6 medium-2" id="top-lang-div"><div id="lang-menu"><?php zume_the_languages( array( 'dropdown' => 1 ) ); ?></div></div>
     <div class="cell small-3 medium-2 show-for-small hide-for-large" id="top-mobile-menu-div">
         <div class="mobile-menu">
-            <a data-toggle="off-canvas" style="cursor:pointer;"><img src="<?php echo esc_url( zume_images_uri() . 'hamburger.svg' ) ?>" alt="menu" /></a>
+            <a data-toggle="off-canvas" style="cursor:pointer; float: right;"><img src="<?php echo esc_url( zume_images_uri() . 'hamburger.svg' ) ?>" alt="menu" /></a>
         </div>
     </div>
-    <div class="cell hide-for-small-only medium-1"></div>
+
 </div>
