19178cef1248b62c134f7ff5e61c8553f6a8052e
Chris Chasm <chris@chasm.solutions>
2020-01-17 10:19:03 -0700

    Swapped Google tag manager

--- header.php
+++ header.php
@@ -5,6 +5,13 @@
   <html class="no-js"  <?php language_attributes(); ?>>
 
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MSGWKV5');</script>
+        <!-- End Google Tag Manager -->
         <meta charset="utf-8">
 
         <!-- Force IE to use the latest rendering engine available -->
@@ -48,23 +55,16 @@
                          src="https://www.facebook.com/tr?id=1552465945075453&ev=PageView&noscript=1"
             /></noscript>
         <!-- End Facebook Pixel Code -->
-
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <?php // @codingStandardsIgnoreStart ?>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-102207784-1"></script>
-        <?php // @codingStandardsIgnoreEnd ?>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-102207784-1');
-        </script>
+        
     </head>
 
     <!-- Uncomment this line if using the Off-Canvas Menu -->
 
     <body <?php body_class(); ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSGWKV5"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
         <div class="off-canvas-wrapper">
 
