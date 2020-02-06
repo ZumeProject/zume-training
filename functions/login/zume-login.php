a22e3f8a0844bdc0a8276d398e68901a42349409
Chris Chasm <chris@chasm.solutions>
2020-02-05 19:19:10 -0700

    Login: Added user automatically to zume.vision site

--- functions/login/zume-login.php
+++ functions/login/zume-login.php
@@ -134,6 +134,7 @@ class Zume_User_Registration
             add_user_meta( $user_id, 'google_sso_email', $user_email, true );
 
             add_user_to_blog( get_current_blog_id(), $user_id, 'subscriber' ); // add user to ZumeProject site.
+            add_user_to_blog( '12', $user_id, 'subscriber' ); // add user to Zume Vision
 
         }
         // if no google_sso_email found but user with email does exist
@@ -152,6 +153,7 @@ class Zume_User_Registration
             }
 
             add_user_to_blog( get_current_blog_id(), $user_id, 'subscriber' ); // add user to ZumeProject site.
+            add_user_to_blog( '12', $user_id, 'subscriber' ); // add user to Zume Vision
 
         }
 
@@ -353,6 +355,7 @@ class Zume_User_Registration
             add_user_meta( $user_id, 'facebook_sso_email', $user_email, true );
 
             add_user_to_blog( get_current_blog_id(), $user_id, 'subscriber' ); // add user to ZumeProject site.
+            add_user_to_blog( '12', $user_id, 'subscriber' ); // add user to Zume Vision
 
         }
         // if no facebook_sso_email found but user with email does exist
@@ -372,6 +375,7 @@ class Zume_User_Registration
             }
 
             add_user_to_blog( get_current_blog_id(), $user_id, 'subscriber' ); // add user to ZumeProject site.
+            add_user_to_blog( '12', $user_id, 'subscriber' ); // add user to Zume Vision
         }
 
 
@@ -668,6 +672,7 @@ class Zume_User_Registration
         add_user_meta( $user_id, 'zume_affiliation_key', null, true );
 
         add_user_to_blog( get_current_blog_id(), $user_id, 'subscriber' ); // add user to ZumeProject site.
+        add_user_to_blog( '12', $user_id, 'subscriber' ); // add user to Zume Vision
 
         // log user in
         $user = get_user_by( 'id', $user_id );
