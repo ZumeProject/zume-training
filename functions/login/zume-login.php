bb9094e9856c2118ae83b0059d505451d05badc5
Chris Chasm <chris@chasm.solutions>
2020-02-07 07:04:07 -0700

    Login: Added alt to image tag

--- functions/login/zume-login.php
+++ functions/login/zume-login.php
@@ -692,7 +692,7 @@ Zume_User_Registration::instance();
 
 function zume_spinner( $size = '15', $echo = true ) {
     if ( $echo ) {
-        ?><img src="<?php echo esc_url( site_url() ) ?>/wp-content/themes/zume-training/assets/images/spinner.svg" width="<?php echo esc_attr( $size ) ?>px" /><?php
+        ?><img src="<?php echo esc_url( site_url() ) ?>/wp-content/themes/zume-training/assets/images/spinner.svg" width="<?php echo esc_attr( $size ) ?>px" alt="spinner" /><?php
     } else {
         return '<img src="'. esc_url( site_url() ) . '/wp-content/themes/zume-training/assets/images/spinner.svg" width="'. $size .'px" />';
     }
