f521557c5a8b3c6c4e23ba1ac457a2cb3e102d9c
Chris Chasm <chris@chasm.solutions>
2020-03-14 09:49:10 -0600

    Add back training transfer function

--- functions/zume-v4-global-network-link.php
+++ functions/zume-v4-global-network-link.php
@@ -1,40 +1,41 @@
 <?php
 
-//add_action('zume_create_group', 'trigger_group_to_training_transfer', 10, 3 );
-//function trigger_group_to_training_transfer( $user_id, $group_key, $group ) {
-//
-//    // build fields for transfer
-//    $fields = [
-//        "title" => $group['group_name'],
-//        "zume_group_id" => $group_key,
-//        "contact_count" => $group['members'],
-//        "leader_count" => 1,
-//        "start_date" => strtotime( $group['created_date'] ),
-//        "status" => "new",
-//    ];
-//
-//    $site = Site_Link_System::get_site_connection_vars( 21033 ); // @todo remove hardcoded
-//    if ( ! $site ) {
-//        return new WP_Error( __METHOD__, 'Missing site to site data' );
-//    }
-//
-//    $args = [
-//        'method' => 'POST',
-//        'body' => json_encode( $fields ),
-//        'headers' => [
-//            'Content-Type' => 'application/json',
-//            'Authorization' => 'Bearer ' . $site['transfer_token'],
-//        ],
-//    ];
-//
-//    $result = wp_remote_post( 'https://' . trailingslashit( $site['url'] ) . 'wp-json/dt-posts/v2/trainings', $args );
-//    if ( is_wp_error( $result ) ) {
-//        return new WP_Error( 'failed_remote_post', $result->get_error_message() );
-//    }
-//
-//    $body = json_decode( $result['body'], true );
-//    dt_write_log($body);
-//
-//    return $body;
-//
-//}
+add_action('zume_create_group', 'trigger_group_to_training_transfer', 10, 3 );
+function trigger_group_to_training_transfer( $user_id, $group_key, $group ) {
+
+    // build fields for transfer
+    $fields = [
+        "title" => $group['group_name'],
+        "zume_group_id" => $group_key,
+        "contact_count" => $group['members'],
+        "leader_count" => 1,
+        "start_date" => strtotime( $group['created_date'] ),
+        "status" => "new",
+    ];
+
+    $site = Site_Link_System::get_site_connection_vars( 21116 ); // @todo remove hardcoded
+    if ( ! $site ) {
+        dt_write_log(__METHOD__ . ' FAILED TO GET SITE LINK TO GLOBAL ');
+        return false;
+    }
+
+    $args = [
+        'method' => 'POST',
+        'body' => json_encode( $fields ),
+        'headers' => [
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . $site['transfer_token'],
+        ],
+    ];
+
+    $result = wp_remote_post( 'https://' . trailingslashit( $site['url'] ) . 'wp-json/dt-posts/v2/trainings', $args );
+    if ( is_wp_error( $result ) ) {
+        dt_write_log(__METHOD__ . ' TO CREATE TRAINING FOR ' . $group['name'] );
+        return false;
+    }
+
+    $body = json_decode( $result['body'], true );
+
+    return $body;
+
+}
