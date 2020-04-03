4118a8a546319ec810fd626198f078923441ec8b
Chris Chasm <chris@chasm.solutions>
2020-04-03 12:43:19 -0600

    JS api added to youtube

--- template-gmo.php
+++ template-gmo.php
@@ -61,6 +61,49 @@ $current_language = zume_current_language();
             open_modal( jQuery(this).data('value') )
         })
     })
+
+
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '253',
+            width: '450',
+            videoId: 'k6AYmR2nPSs',
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    var done = false;
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+            setTimeout(stopVideo, 6000);
+            done = true;
+        }
+    }
+    function stopVideo() {
+        player.stopVideo();
+    }
+
 </script>
 <div id="page-content" class="padding-top-1">
 
@@ -80,8 +123,8 @@ $current_language = zume_current_language();
                     <div class="center">
                         <p><?php esc_html_e( "Watch this important video explaining the 4 relationships of your new life.", 'zume' ) ?></p>
                     </div>
-                    <div class="center small-video" style="border:1px solid #e6e6e6">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/k6AYmR2nPSs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="center small-video">
+                        <div id="player"></div>
                     </div>
 
                     <br>
