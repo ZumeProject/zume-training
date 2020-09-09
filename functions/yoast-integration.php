fa22118a6eadabf15dbb901db65d9bd3148aec48
Chris Chasm <chris@chasm.solutions>
2020-09-09 15:56:33 -0600

    type to share image

--- functions/yoast-integration.php
+++ functions/yoast-integration.php
@@ -5,7 +5,7 @@ function default_facebook_image( $opengraph_image ) {
     if ( 'page' === $post->post_type ) {
         $tool_id = zume_landing_page_post_id( (int) zume_get_translation( $post->ID, 'en' ) );
         if ( ! empty( $tool_id ) ) {
-            $opengraph_image->add_image( get_template_directory_uri() . '/assets/images/piece-seo-images/'.esc_attr( $tool_id ).'.jpg' );
+            $opengraph_image->add_image( get_template_directory_uri() . '/assets/images/pieces-seo-images/'.esc_attr( $tool_id ).'.jpg' );
         }
     }
 }
