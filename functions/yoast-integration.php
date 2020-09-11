c0c2204c8b4f2dcbe66cd755f1c15892d016fd7c
Chris Chasm <chris@chasm.solutions>
2020-09-11 17:58:35 -0600

    isset protection on function

--- functions/yoast-integration.php
+++ functions/yoast-integration.php
@@ -2,7 +2,7 @@
 
 function default_facebook_image( $opengraph_image ) {
     global $post;
-    if ( 'page' === $post->post_type ) {
+    if ( isset( $post->post_type ) && 'page' === $post->post_type ) {
         $tool_id = zume_landing_page_post_id( (int) zume_get_translation( $post->ID, 'en' ) );
         $opengraph_image->add_image( get_template_directory_uri() . '/assets/images/pieces-seo-images/'.esc_attr( $tool_id ).'.jpg' );
     }
