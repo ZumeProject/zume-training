d2257e27de048c6209f07813dd9597d16472eee4
Chris Chasm <chris@chasm.solutions>
2020-12-03 07:05:50 -0700

    Mapping Upgrade

--- dt-mapping/geocode-api/mapbox-api.php
+++ dt-mapping/geocode-api/mapbox-api.php
@@ -267,6 +267,9 @@ if ( ! class_exists( 'DT_Mapbox_API' ) ) {
                     $post_record = false;
                 }
 
+                if ( ! function_exists( 'dt_get_location_grid_mirror' ) ) {
+                    require_once( get_template_directory() . '/dt-mapping/globals.php' );
+                }
 
                 wp_enqueue_script( 'mapbox-search-widget', trailingslashit( get_stylesheet_directory_uri() ) . 'dt-mapping/geocode-api/mapbox-search-widget.js', [ 'jquery', 'mapbox-gl', 'shared-functions' ], filemtime( get_template_directory() . '/dt-mapping/geocode-api/mapbox-search-widget.js' ), true );
                 wp_localize_script(
@@ -275,11 +278,17 @@ if ( ! class_exists( 'DT_Mapbox_API' ) ) {
                         "post_id" => $post->ID ?? 0,
                         "post" => $post_record ?? false,
                         "map_key" => self::get_key(),
+                        "mirror_source" => dt_get_location_grid_mirror( true ),
                         "google_map_key" => ( Disciple_Tools_Google_Geocode_API::get_key() ) ? Disciple_Tools_Google_Geocode_API::get_key() : false,
                         "spinner_url" => get_stylesheet_directory_uri() . '/spinner.svg',
                         "theme_uri" => get_stylesheet_directory_uri(),
                         "translations" => array(
-                            'add' => __( 'add', 'disciple-tools' )
+                            'add' => __( 'add', 'disciple-tools' ),
+                            'use' => __( 'Use', 'disciple-tools' ),
+                            'search_location' => __( 'Search Location', 'disciple-tools' ),
+                            'delete_location' => __( 'Delete Location', 'disciple-tools' ),
+                            'open_mapping' => __( 'Open Mapping', 'disciple-tools' ),
+                            'clear' => __( 'clear', 'disciple-tools' )
                         )
                     )
                 );
@@ -418,7 +427,11 @@ if ( ! class_exists( 'DT_Mapbox_API' ) ) {
                 $message = 'Successfully connected to selected source.';
             } else {
                 $status_class = 'not-connected';
-                $message = 'API NOT AVAILABLE';
+                if ( empty( $key )){
+                    $message = 'Please add a Mapbox API Token';
+                } else {
+                    $message = 'Could not connect to the Mapbox API or could not verify the token';
+                }
             }
             ?>
             <form method="post">
@@ -467,15 +480,23 @@ if ( ! class_exists( 'DT_Mapbox_API' ) ) {
                                         Paste the token into the "Mapbox API Token" field in the box above.
                                     </li>
                                 </ol>
+                            <?php else :
+                                global $wpdb;
+                                $location_wo_meta = $wpdb->get_var( "SELECT count(*) FROM $wpdb->postmeta WHERE meta_key = 'location_grid' AND meta_id NOT IN (SELECT DISTINCT( postmeta_id_location_grid ) FROM $wpdb->dt_location_grid_meta) AND meta_value >= 100000000" );
+                                $user_location_wo_meta = $wpdb->get_var( $wpdb->prepare( "SELECT count(*) FROM $wpdb->usermeta WHERE meta_key = %s AND umeta_id NOT IN (SELECT DISTINCT( postmeta_id_location_grid ) FROM $wpdb->dt_location_grid_meta ) AND meta_value >= 100000000", $wpdb->prefix . 'location_grid' ) );
+                                if ( !empty( $location_wo_meta ) || !empty( $user_location_wo_meta ) ) : ?>
+                                    <p class="not-connected">
+                                        <strong>Next:</strong> Please upgrade Users, Contacts and Groups below for the Locations to show up on maps and charts.
+                                    </p>
+                                <?php endif; ?>
                             <?php endif; ?>
-
                         </td>
                     </tr>
                     </tbody>
                 </table>
             </form>
             <style>
-                .connected{ padding: 10px; background-color: lightgreen;}
+                #reachable_source.connected{ padding: 0 10px 0 0; color: darkgreen; background-color: transparent}
                 .not-connected { padding: 10px; background-color: lightcoral; }
             </style>
             <br>
