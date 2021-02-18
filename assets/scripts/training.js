a321755ae16eebc0f1ab95f690de5f67523d85d2
Chris Chasm <chris@chasm.solutions>
2021-02-18 09:47:51 -0700

    fix cookie reader

--- assets/scripts/training.js
+++ assets/scripts/training.js
@@ -67,6 +67,9 @@ jQuery(document).ready(function(){
   if ( getCookie( 'extra' ) === "on") {
     jQuery('#extra_button').removeClass('hollow')
     jQuery('.hide-extra').show();
+  } else if ( getCookie( 'extra' ) === "off" ){
+    jQuery('#extra_button').addClass('hollow')
+    jQuery('.hide-extra').hide();
   }
   if ( getCookie( 'columns' ) === "single") {
     jQuery('#column_button').removeClass('hollow')
