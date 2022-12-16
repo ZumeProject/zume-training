32bd7519720d40012d31979c53e2d2de6af1b0a4
Chris Chasm <chris@chasm.solutions>
2022-12-16 07:37:39 -0700

    Remove Zendesk

--- footer.php
+++ footer.php
@@ -58,19 +58,6 @@
 
 <?php wp_footer(); ?>
 
-<?php
-/**
- * Enable Zendesk by setting this variable to true at the top of the template or page.
- */
-global $zendesk_enable;
-if ( ! empty( $zendesk_enable ) && ( is_user_logged_in() || 'template-zume-login.php' !== basename( get_page_template() ) ) ) : ?>
-<!-- Start of Zendesk Widget script -->
-<script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","zume.zendesk.com");/*]]>*/</script>
-<!-- End of Zendesk Widget script -->
-<?php endif; ?>
-
-
-
 
 </body>
 </html> <!-- end page -->
