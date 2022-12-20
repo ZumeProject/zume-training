8cd364a33a32d7378f5d6ca5d707efb4a7d1b6f5
Chris Chasm <chris@chasm.solutions>
2022-12-20 16:13:05 -0700

    changed permissions

--- functions/post-types/pieces-page-metabox.php
+++ functions/post-types/pieces-page-metabox.php
@@ -74,7 +74,7 @@ function zume_pieces_save( $post_id ) {
     }
 
     // if our current user can't edit this post, bail
-    if ( !current_user_can( 'edit_post' ) ) { return;
+    if ( !current_user_can( 'edit_posts' ) ) { return;
     }
 
     if ( isset( $_POST['zume_piece'] ) ) {
